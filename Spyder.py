dca18e1c7562a83ad2172ac6d3b9e9157d23d9e3
motty6921 <52720453+motty6921@users.noreply.github.com>
2019-09-21 00:41:49 -0700

    Script

---
+++ Spyder.py
@@ -0,0 +1,13 @@
+import sys #Sys
+import time #Time
+import os #This gives me the ability to open Command Prompt
+
+time.sleep(0.5) #Stops the program from running for half (0.5) of a second
+
+#'os.system' opens the CM
+os.system("start /wait cmd /c spyder3")
+
+time.sleep(2) #Stops the program from exiting for 2 seconds
+
+exit()
+
